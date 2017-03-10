1f054b43dd199a68e10e9b41578c295cd0676770
Тимур Макаров <tmakarov95@gmail.com>
2017-03-11 01:12:15 +0300

    add(App component): push-top to container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from '../Header'
 // import './style.css';
 
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <div className="container">
-        	{this.props.children}
+        <Header/>
+        <div className="container push-top-40">
+          {this.props.children}
         </div>
       </div>
     );
