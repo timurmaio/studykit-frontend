f4cf77ce83aa5ab82e6f244a5ebfb2ac6be3c2aa
Тимур Макаров <tmakarov95@gmail.com>
2017-03-11 01:09:08 +0300

    add: Course component

---
+++ src/components/Course/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class Course extends Component {
+  render() {
+    return (
+      <div className="course shadow push-bottom-30">
+        <img className="course_img" src={this.props.img} alt="Изображение курса"/>
+        <h5 className="course_name push-left-20 push-top-10">{this.props.name}</h5>
+        <p className="course_desc push-left-20">{this.props.desc}</p>
+      </div>
+    )
+  }
+}
+
+export default Course;
