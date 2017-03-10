9b7258bf47104cf6c0461550d37f042ba6ef74bc
Тимур Макаров <tmakarov95@gmail.com>
2017-03-11 01:11:13 +0300

    update(Courses component): mount Sidebar and fetched Course components

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import API_URL from '../../helpers/config';
+import h from '../../helpers/course_h';
+
+import Sidebar from '../Sidebar';
+import Course from '../Course';
 
 class Courses extends Component {
-	render() {
-		return <h1>This is Courses</h1>
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: []
+    };
+  }
+  componentDidMount() {
+    let that = this;
+    h.getCourses().then(function(data) {
+      that.setState({courses: data});
+    });
+  }
+  renderCourse(course) {
+    return (
+      <div key={course.id} className="col-xl-4">
+        <Course name={course.title} desc={course.description} img={API_URL + course.avatar.url}/>
+      </div>
+    )
+  }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xl-3">
+          <Sidebar/>
+        </div>
+        <div className="col-xl-9 ">
+          <div className="row">
+            {this.state.courses.map(this.renderCourse)}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Courses;
