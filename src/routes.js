76920b27a3082d1620f5c7500ee442e9815a748d
Тимур Макаров <tmakarov95@gmail.com>
2017-04-20 22:35:16 +0300

    update: routes

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 
-import App from './components/App';
-import Profile from './components/Profile';
-import Courses from './components/Courses';
-import Authentication from './components/Authentication';
-import MainLayout from './components/MainLayout';
+import App from './components/App'
+import Profile from './components/Profile'
+import Courses from './components/Courses'
+import Course from './components/Course'
+import NewCourse from './components/NewCourse'
+import ShowContent from './components/ShowContent'
+import Authentication from './components/Authentication'
+import MainLayout from './components/MainLayout'
 
-import NotFound from './components/NotFound';
+import NotFound from './components/NotFound'
 
 const routes = (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route component={App}>
-        <Route path="/" component={Courses}/>
-        <Route path="/profile" component={Profile}/>
+        <Route path='/' component={Courses} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/courses/:id' component={Course} />
+        <Route path='/courses/:id/contents/new' component={NewCourse} />
+        <Route path='/courses/:id/contents/:contentId' component={ShowContent} />
       </Route>
-      <Route path="/login" component={Authentication}/>
+      <Route path='/login' component={Authentication} />
     </Route>
-    <Route path="*" component={NotFound}/>
+    <Route path='*' component={NotFound} />
   </Router>
-);
+)
 
-export default routes;
+export default routes
