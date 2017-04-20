4b4cc1742b416f3e35bce2a411ca0865f4f5e961
Тимур Макаров <tmakarov95@gmail.com>
2017-04-20 22:33:44 +0300

    refactor: Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,21 +15,21 @@ class Header extends Component {
       :
       <Link to='/login' className="profile__link push-left-10 btn btn-default btn-danger">Войти</Link>
     return (
-      <header className='shadow'>
-        <div className='container'>
-          <div className='flex flex--vcenter height-60'>
-            <h1 className='logo'>Studykit</h1>
-            <img src={logo} width="30px" alt=""/>
+      <header className="shadow">
+        <div className="container">
+          <div className="flex flex--vcenter height-60">
+            <h1 className="logo">Studykit</h1>
+            <img src={logo} width="30px" alt="" />
 
-            <Link to='/' activeClassName='link--active' className='link push-left-50'>Курсы</Link>
-            <Link to='/calendar' activeClassName='link--active' className='link push-left-10'>Календарь</Link>
-            <Link to='/group' activeClassName='link--active' className='link push-left-10'>Группа</Link>
-            <Link to='/news' activeClassName='link--active' className='link push-left-10'>Новости</Link>
+            <Link to='/' activeClassName="link--active" className="link push-left-50">Курсы</Link>
+            <Link to='/calendar' activeClassName="link--active" className="link push-left-10">Календарь</Link>
+            <Link to='/group' activeClassName="link--active" className="link push-left-10">Группа</Link>
+            <Link to='/news' activeClassName="link--active" className="link push-left-10">Новости</Link>
             <div className='profile push-left'>
 
-              <Link to='/profile' activeClassName='profile__link--active' className='profile__link'>
-              <img src={panel} width="20px" className="mr-2" alt=""/>
-              Панель
+              <Link to='/profile' activeClassName="profile__link--active" className="profile__link">
+                <img src={panel} width="20px" className="mr-2" alt="" />
+                Панель
               </Link>
               {signInOut}
             </div>
