adac8f632155f5f218d4cecdbdcaad902fd4acf6
Тимур Макаров <tmakarov95@gmail.com>
2017-04-20 22:34:33 +0300

    add: config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import _axios from 'axios'
+
+const API_URL = 'http://46.101.216.31'
+// const API_URL = 'http://localhost:3000'
+
+const axios = _axios.create({
+  headers: { 'Authorization': localStorage.getItem('jwt_token') }
+})
+
+export { API_URL, axios }
