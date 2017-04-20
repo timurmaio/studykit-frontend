72138ea46c25fbaa0fdc3c81336e6686e7227de5
Тимур Макаров <tmakarov95@gmail.com>
2017-04-20 22:27:51 +0300

    add: Course component

--- src/components/Course/index.js
+++ src/components/Course/index.js
@@ -1,15 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { API_URL, axios } from '../../config'
 
 class Course extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: '',
+      description: '',
+      courseId: '',
+      content: [],
+      course: {},
+      owner: {}
+    }
+  }
+
+  componentDidMount () {
+    axios.get(API_URL + '/api/courses/' + this.props.params.id).then((response) => {
+      this.setState({ title: response.data.title, description: response.data.description, courseId: response.data.id, content: response.data.content, course: response.data, owner: response.data.owner })
+      console.log(response.data)
+    })
+  }
+
+  renderItem = (item) => {
     return (
-      <div className="course shadow push-bottom-30">
-        <img className="course_img" src={this.props.img} alt="Изображение курса"/>
-        <h5 className="course_name push-left-20 push-top-10">{this.props.name}</h5>
-        <p className="course_desc push-left-20">{this.props.desc}</p>
+      <div key={item.id} className="col-md-4 mt-3">
+        <div className="card">
+          <div className="card-block">
+            <h3 className="card-title">{item.title}</h3>
+            <p className="card-text">{item.serial_number}</p>
+            <Link to={{ pathname: `/courses/${this.state.courseId}/contents/${item.id}` }} className="btn btn-success">Просмотреть</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  render () {
+    const style = {
+      marginTop: '-20px'
+    }
+    return (
+      <div style={style}>
+
+        <div className="row">
+          <div className="col-md-3">
+            <h1>{this.state.title}</h1>
+            <img src={this.state.course.avatar} alt="Изображние курса" width="150px" />
+            <p>{this.state.description}</p>
+            <b>Владелец:</b>
+            <p>{this.state.owner.first_name} {this.state.owner.last_name}</p>
+
+            <Link to={{ pathname: `/courses/${this.state.courseId}/contents/new` }} className="btn btn-primary mt-3">Добавить лекцию</Link>
+          </div>
+          <div className="col-md-9">
+            <div className="row">
+              {this.state.content.map(this.renderItem)}
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
 }
 
-export default Course;
+export default Course
