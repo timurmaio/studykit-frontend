ca7bf34674c8af738bd2201e2c25a874129da14b
Тимур Макаров <tmakarov95@gmail.com>
2017-05-23 17:24:32 +0300

    refactor: Authentication

--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -3,83 +3,114 @@ import axios from 'axios'
 import { Link, browserHistory } from 'react-router'
 
 import { API_URL } from '../../config'
+const signInUrl = `${API_URL}/api/users/login`
+const signUpUrl = `${API_URL}/api/users/register`
 
-const url = `${API_URL}/api/users/login`
+// const axios = createAxios()
 
 class Authentication extends Component {
   constructor (props) {
     super(props)
+
     this.state = {
-      value: '',
+      password: '',
+      email: '',
+      signUp: false,
       error: ''
     }
   }
 
   handleChange = (event) => {
-    this.setState({ value: event.target.value })
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      [name]: value
+    })
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
     let userData = {
       user: {
-        email: this.refs.email.value,
-        password: this.refs.password.value
+        email: this.state.email,
+        password: this.state.password
       }
     }
-    console.log(url)
-    console.log(userData)
+    // console.log(url)
+    // console.log(userData)
     let that = this
-    axios.post(url, userData).then(function (response) {
+    axios.post(signInUrl, userData).then(function (response) {
       console.log(response)
       if (response.status === 200) {
         console.log('Everything is OK')
         console.log(response.data.jwt_token)
         // localStorage.setItem('isAuthenticated', true)
         localStorage.setItem('jwt_token', response.data.jwt_token)
-        localStorage.setItem('user_id', response.data.id)
+        // localStorage.setItem('user_id', response.data.id)
         browserHistory.push('/')
       }
     }).catch(function (error) {
-      that.setState({ error: 'Пользователя не существует' })
+      if (error.response.status === 404) {
+        that.setState({ error: '404 Ошибка подключения' })
+      } else {
+        that.setState({ error: 'Пользователь не найден' })
+      }
+      // that.state.error = error
       // that.refs.alert.value = error;
-      console.log(error)
+      // console.log(error)
     })
   }
 
-  render () {
-    const divStyle = {
-      marginTop: '50%',
-      color: 'white'
-    }
+  changeFormType = (event) => {
+    event.preventDefault()
+    this.setState({
+      signUp: !this.state.signUp
+    })
+  }
+
+  renderSignUpForm = () => {
     return (
-      <div className='block'>
-        <div className='container'>
-          <div className='row'>
-            <div className='offset-xl-4 col-xl-4'>
-              <div style={divStyle}>
-                <h1>Authentication</h1>
-              </div>
-              <form onSubmit={this.handleSubmit} className='form shadow'>
-                <div>
-                  <label>
-                    Email:
-                    <input className='input' type='text' ref='email' onChange={this.handleChange} placeholder='Электронная почта' />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    Пароль:
-                    <input className='input' type='password' ref='password' placeholder='Пароль' />
-                  </label>
-                </div>
-                <input className='' type='submit' value='Войти' />
-                <Link to='/'>Главная</Link>
-                <div className='alert' ref='alert'>{this.state.error}</div>
-              </form>
-            </div>
+      <div>
+        <label htmlFor="firstName">Имя:</label>
+        <input id="firstName" name="firstName" className="input" type="text" onChange={this.handleChange} placeholder="Имя" />
+      </div>
+    )
+  }
+
+  render () {
+    const authButtonText = this.state.signUp ? 'Зарегистрироваться' : 'Войти'
+    const changeAuthButtonText = this.state.signUp ? 'Вход' : 'Регистрация'
+
+    return (
+      <div className="authentication">
+        <h1>Вход в систему</h1>
+        <form onSubmit={this.handleSubmit} className="form shadow">
+
+          <div>
+            <label htmlFor="email">Электронная почта:</label>
+            <input id="email" name="email" className="input" type="text" onChange={this.handleChange} placeholder="Электронная почта" />
           </div>
-        </div>
+
+          {this.state.signUp ? this.renderSignUpForm() : null}
+
+          <div>
+            <label htmlFor="password">Пароль:</label>
+            <input id="password" name="password" className="input" type="password" onChange={this.handleChange} placeholder="Пароль" />
+          </div>
+
+          <input type="submit" value={authButtonText} />
+
+          <div>
+            <button onClick={this.changeFormType}>{changeAuthButtonText}</button>
+          </div>
+
+          <div>
+            <Link to="/">Перейти к курсам</Link>
+          </div>
+
+          <div className="alert">{this.state.error}</div>
+        </form>
       </div>
     )
   }
