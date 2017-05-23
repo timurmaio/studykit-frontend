296117c3c3ded09a45e417a2ed2e28d38ef90193
Тимур Макаров <tmakarov95@gmail.com>
2017-05-23 19:49:46 +0300

    refactor(routes.js): single quotes to double, add spacings

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router, Route, browserHistory} from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 import App from './components/App'
 import Profile from './components/Profile'
@@ -10,24 +10,23 @@ import ShowContent from './components/ShowContent'
 import Authentication from './components/Authentication'
 import MainLayout from './components/MainLayout'
 import News from './components/News'
-
 import NotFound from './components/NotFound'
 
 const routes = (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route component={App}>
-        <Route path='/' component={Courses} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/profile/courses/:id' component={Course} />
-        <Route path='/news' component={News} />
-        <Route path='/courses/:id' component={Course} />
-        <Route path='/courses/:id/contents/new' component={NewCourse} />
-        <Route path='/courses/:id/contents/:contentId' component={ShowContent} />
+        <Route path="/" component={Courses} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/profile/courses/:id" component={Course} />
+        <Route path="/news" component={News} />
+        <Route path="/courses/:id" component={Course} />
+        <Route path="/courses/:id/contents/new" component={NewCourse} />
+        <Route path="/courses/:id/contents/:contentId" component={ShowContent} />
       </Route>
-      <Route path='/login' component={Authentication} />
+      <Route path="/login" component={Authentication} />
     </Route>
-    <Route path='*' component={NotFound} />
+    <Route path="*" component={NotFound} />
   </Router>
 )
 
