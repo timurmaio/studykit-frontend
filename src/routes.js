1a90149f47ab428f87da23b533dcd3001fffaab3
Тимур Макаров <tmakarov95@gmail.com>
2017-05-28 13:27:13 +0300

    update: routes

--- src/routes.js
+++ src/routes.js
@@ -8,25 +8,20 @@ import Course from './components/Course'
 import NewCourse from './components/NewCourse'
 import ShowContent from './components/ShowContent'
 import Authentication from './components/Authentication'
-import MainLayout from './components/MainLayout'
-import News from './components/News'
 import NotFound from './components/NotFound'
 
 const routes = (
   <Router history={browserHistory}>
-    <Route component={MainLayout}>
-      <Route component={App}>
-        <Route path="/" component={Courses} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/profile/courses/:id" component={Course} />
-        <Route path="/news" component={News} />
-        <Route path="/courses/:id" component={Course} />
-        <Route path="/courses/:id/contents/new" component={NewCourse} />
-        <Route path="/courses/:id/contents/:contentId" component={ShowContent} />
-      </Route>
+    <Route component={App}>
       <Route path="/login" component={Authentication} />
+      <Route path="/" component={Courses} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/profile/courses/:id" component={Course} />
+      <Route path="/courses/:id" component={Course} />
+      <Route path="/courses/:id/contents/new" component={NewCourse} />
+      <Route path="/courses/:id/contents/:contentId" component={ShowContent} />
+      <Route path="*" component={NotFound} />
     </Route>
-    <Route path="*" component={NotFound} />
   </Router>
 )
 
