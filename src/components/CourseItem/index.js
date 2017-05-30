4853311b57a8d4318cc407975441ca6bddc34e9e
Тимур Макаров <tmakarov95@gmail.com>
2017-05-30 15:35:12 +0300

    add(CourseItem): border-radius, box-shadow, transition

--- src/components/CourseItem/index.js
+++ src/components/CourseItem/index.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
 class CourseItem extends Component {
   render () {
     return (
-      <div className="course shadow push-bottom-30">
-        <img className="course_img" src={this.props.img} alt="Изображение курса" />
-        <h5 className="course_name push-left-20 push-top-10">{this.props.name}</h5>
-        <p className="course_desc push-left-20">{this.props.desc}</p>
-        <Link to={{ pathname: `courses/${this.props.id}` }} className="push-left-20">Подробнее</Link>
-      </div>
+      <Link to={{ pathname: `courses/${this.props.id}` }}>
+        <div className="course shadow push-bottom-30">
+          <img className="course_img" src={this.props.img} alt="Изображение курса" />
+          <h5 className="course_name push-left-20 push-top-10">{this.props.name}</h5>
+          <p className="course_desc push-left-20">{this.props.desc}</p>
+        </div>
+      </Link>
     )
   }
 }
