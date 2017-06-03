2fd573e523d1a4a9c13f3acfc9abd9d462a54ad3
Тимур Макаров <tmakarov95@gmail.com>
2017-06-03 20:15:48 +0300

    remove(index.js): comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
-import './index.css' // Styles
-import routes from './routes' // Routes
+import './index.css'
+import routes from './routes'
 
 ReactDOM.render(routes, document.getElementById('root'))
-// registerServiceWorker()
+registerServiceWorker()
