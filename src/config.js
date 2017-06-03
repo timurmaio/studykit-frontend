e4fb2dea1120dcb83978fd3be6a9fb6d199401fb
Тимур Макаров <tmakarov95@gmail.com>
2017-06-03 20:10:45 +0300

    remove(config): axios instance

--- src/config.js
+++ src/config.js
@@ -3,14 +3,10 @@ import _axios from 'axios'
 const API_URL = 'http://46.101.216.31:3000'
 // const API_URL = 'http://localhost:3000'
 
-const axios = _axios.create({
-  headers: { 'Authorization': localStorage.getItem('jwt_token') }
-})
-
 const createAxios = () => {
   return _axios.create({
     headers: { 'Authorization': localStorage.getItem('jwt_token') }
   })
 }
 
-export { API_URL, axios, createAxios }
+export { API_URL, createAxios }
