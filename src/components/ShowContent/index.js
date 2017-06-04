d83adaf5c497dbe8adf614bdd04599fa6ac458f6
Тимур Макаров <tmakarov95@gmail.com>
2017-06-04 04:04:02 +0300

    update(ShowContent): full remake

--- src/components/ShowContent/index.js
+++ src/components/ShowContent/index.js
@@ -8,20 +8,44 @@ class ShowContent extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      courseId: '',
-      content: []
+      title: '',
+      description: ''
     }
   }
 
   componentDidMount () {
-    axios.get(API_URL + '/api/courses/' + this.props.params.id + '/content/' + this.props.params.contentId).then((response) => {
+    axios.get(`${API_URL}/api/lectures/${this.props.params.lectureId}/content/${this.props.params.contentId}`).then((response) => {
       console.log(response.data)
-      this.setState({ courseId: response.data.course_id, content: response.data })
+      this.setState({ title: response.data.title, description: response.data.body })
     })
   }
 
   render () {
-    return <ReactMarkdown source={this.state.content.body} />
+    const input = this.state.description
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <div className="panel h-600">
+
+            </div>
+          </div>
+          <div className="col-9">
+            <div className="panel h-600">
+              <header className="ml-32 mt-24 fs-24 mb-20">{this.state.title}</header>
+              <ReactMarkdown className="mx-32" source={input} />
+              <div className="form-group mx-32">
+                <label htmlFor="exampleTextarea">Введите сюда своё решение</label>
+                <textarea className="form-control" id="exampleTextarea" rows="6"></textarea>
+              </div>
+              {/*<textarea className="input mx-32" name="" id=""></textarea>*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    // return <ReactMarkdown source={this.state.content.body} />
   }
 }
 
