9aada5ef751abe39e91c17154ee3573702309437
Тимур Макаров <tmakarov95@gmail.com>
2017-06-04 04:00:48 +0300

    update(Profile): SignOut button, some view

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+import { API_URL, createAxios } from '../../config'
 
 class Profile extends Component {
   constructor (props) {
     super(props)
+  }
+
+  componentDidMount () {
+
+  }
+
+  handleSignOut = () => {
+    localStorage.removeItem('jwt_token')
+    localStorage.removeItem('user_id')
+    browserHistory.push('/signin')
   }
 
   render () {
@@ -10,13 +22,17 @@ class Profile extends Component {
       <div className="container">
         <div className="row">
           <div className="col-3">
-            <img src="" alt=""/>
-            <button>Изменить фото</button>
-            <button>Выйти</button>
+            <div className="panel h-600">
+              <img src="" alt=""/>
+              <button>Изменить фото</button>
+              <button onClick={this.handleSignOut}>Выйти</button>
+            </div>
           </div>
           <div className="col-9">
-            <header>Личные данные</header>
-            <button>Изменить</button>
+            <div className="panel h-600">
+              <header>Личные данные</header>
+              <button>Изменить</button>
+            </div>
           </div>
         </div>
       </div>
