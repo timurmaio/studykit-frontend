722ac9294f9c8a19257bf2eb7f7ba1246b4b299d
Тимур Макаров <tmakarov95@gmail.com>
2017-06-04 03:58:38 +0300

    update(CourseCard): new classes

--- src/components/CourseCard/index.js
+++ src/components/CourseCard/index.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router'
 
 const CourseCard = (props) => {
   return (
-    <Link to={{ pathname: `courses/${props.id}` }}>
-      <div className="course shadow push-bottom-30">
-        <img className="course_img" src={props.avatar} alt="Изображение курса" />
-        <h5 className="course_name push-left-20 push-top-10">{props.title}</h5>
-        <p className="course_desc push-left-20">{props.description}</p>
+    <Link to={`courses/${props.id}`} className="link link--black">
+      <div className="card">
+        <img className="card_image" src={props.avatar} alt="Изображение курса" />
+        <div className="card_body">
+          <header className="card_title">{props.title}</header>
+          <p className="card_description">{props.description}</p>
+        </div>
       </div>
     </Link>
   )
