ae8e5a24af6ed4e4835267a879ccc257c5388e67
Тимур Макаров <timurmaio@gmail.com>
2018-03-14 23:48:53 +0300

    move to npms boostrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import './index.css'
 import routes from './routes'
 
